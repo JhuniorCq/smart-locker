89d8958707ae5c099455beeb9693f6c438c35410
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-12-13 05:55:59 -0500

    Fin

--- src/credentials.js
+++ src/credentials.js
@@ -7,12 +7,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7HDCxqQZnvT90SfgZIUTm7dYw4Wsowo8",
-  authDomain: "practicando-72e06.firebaseapp.com",
-  projectId: "practicando-72e06",
-  storageBucket: "practicando-72e06.firebasestorage.app",
-  messagingSenderId: "564287319239",
-  appId: "1:564287319239:web:d73d9b84c1c56f7648e2e2",
+  apiKey: "AIzaSyDd5xxFUl98PsBlyin0QDjGexQIOOQWk5o",
+  authDomain: "smart-locker-92c21.firebaseapp.com",
+  projectId: "smart-locker-92c21",
+  storageBucket: "smart-locker-92c21.firebasestorage.app",
+  messagingSenderId: "290380660903",
+  appId: "1:290380660903:web:0bd63f84510cc8ebce1b82",
+  // apiKey: "AIzaSyB7HDCxqQZnvT90SfgZIUTm7dYw4Wsowo8",
+  // authDomain: "practicando-72e06.firebaseapp.com",
+  // projectId: "practicando-72e06",
+  // storageBucket: "practicando-72e06.firebasestorage.app",
+  // messagingSenderId: "564287319239",
+  // appId: "1:564287319239:web:d73d9b84c1c56f7648e2e2",
 };
 
 // Initialize Firebase
