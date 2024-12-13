25596dcee1c85c9185d8d1452481da33e62eb6de
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-12-13 05:57:08 -0500

    .

--- src/credentials.js
+++ src/credentials.js
@@ -13,12 +13,6 @@ const firebaseConfig = {
   storageBucket: "smart-locker-92c21.firebasestorage.app",
   messagingSenderId: "290380660903",
   appId: "1:290380660903:web:0bd63f84510cc8ebce1b82",
-  // apiKey: "AIzaSyB7HDCxqQZnvT90SfgZIUTm7dYw4Wsowo8",
-  // authDomain: "practicando-72e06.firebaseapp.com",
-  // projectId: "practicando-72e06",
-  // storageBucket: "practicando-72e06.firebasestorage.app",
-  // messagingSenderId: "564287319239",
-  // appId: "1:564287319239:web:d73d9b84c1c56f7648e2e2",
 };
 
 // Initialize Firebase
